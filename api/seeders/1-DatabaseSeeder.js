12b1734c33aea2fa337116f82899226d2460f049
Klari <szappan.klarissza@gmail.com>
2022-07-14 18:24:09 +0200

    Remove unnecessary comments

--- api/seeders/1-DatabaseSeeder.js
+++ api/seeders/1-DatabaseSeeder.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Faker dokumentáció, API referencia: https://fakerjs.dev/guide/#node-js
 const { faker } = require('@faker-js/faker');
 const chalk = require("chalk");
 const models = require("../models");
@@ -42,18 +41,11 @@ module.exports = {
                 })
             }
 
-            console.log(chalk.green("A DatabaseSeeder lefutott"));
+            console.log(chalk.green("The DatabaseSeeder terminated successfully."));
         } catch (e) {
-            // Ha a seederben valamilyen hiba van, akkor alapértelmezés szerint elég szegényesen írja
-            // ki azokat a rendszer a seeder futtatásakor. Ezért ez Neked egy segítség, hogy láthasd a
-            // hiba részletes kiírását.
-            // Így ha valamit elrontasz a seederben, azt könnyebben tudod debug-olni.
-            console.log(chalk.red("A DatabaseSeeder nem futott le teljesen, mivel az alábbi hiba történt:"));
+            console.log(chalk.red("Some error occured:"));
             console.log(chalk.gray(e));
         }
     },
-
-    // Erre alapvetően nincs szükséged, mivel a parancsok úgy vannak felépítve,
-    // hogy tiszta adatbázist generálnak, vagyis a korábbi adatok enélkül is elvesznek
     down: async (queryInterface, Sequelize) => {},
 };
